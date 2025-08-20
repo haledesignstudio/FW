ecea3cf26a374cefee27efa68f39a5808d2d5e75
Malcolm <mcscarrott01@gmail.com>
2025-08-20 22:28:11 +0200

    main

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -117,13 +117,13 @@ const Footer: React.FC = () => {
             id: 4,
             content: (
                 <ul className="text-[2vh] [@media(min-width:768px)_and_(min-aspect-ratio:1/1)]:text-[1.5vh] [@media(max-height:600px)_and_(max-width:768px)]:text-[8vh] leading-tight">
-                    <li><a href="/contact">{shouldAnimate ? <FutureText text="Contact" delay={2000} speed={20} triggerOnVisible={false} /> : "Contact"}</a></li>
+                    <li><Link href="/contact">{shouldAnimate ? <FutureText text="Contact" delay={2000} speed={20} triggerOnVisible={false} /> : "Contact"}</Link></li>
                     <li><Link href="/keynotes">{shouldAnimate ? <FutureText text="Keynotes" delay={2000} speed={20} triggerOnVisible={false} /> : "Keynotes"}</Link></li>
-                    <li><a href="/people">{shouldAnimate ? <FutureText text="Join us" delay={2000} speed={20} triggerOnVisible={false} /> : "Join us"}</a></li>
-                    <li><a href="/faq">{shouldAnimate ? <FutureText text="FAQs" delay={2000} speed={20} triggerOnVisible={false} /> : "FAQs"}</a></li>
-                    <li><a href="/privacy-policy">{shouldAnimate ? <FutureText text="Privacy policy" delay={2000} speed={20} triggerOnVisible={false} /> : "Privacy policy"}</a></li>
-                    <li><a href="/terms-conditions">{shouldAnimate ? <FutureText text="Terms and conditions" delay={2000} speed={20} triggerOnVisible={false} /> : "Terms and conditions"}</a></li>
-                    <li><a href="/insights">{shouldAnimate ? <FutureText text="Shareholder Value Analytics" delay={2000} speed={20} triggerOnVisible={false} /> : "Shareholder Value Analytics"}</a></li>
+                    <li><Link href="/people">{shouldAnimate ? <FutureText text="Join us" delay={2000} speed={20} triggerOnVisible={false} /> : "Join us"}</Link></li>
+                    <li><Link href="/faq">{shouldAnimate ? <FutureText text="FAQs" delay={2000} speed={20} triggerOnVisible={false} /> : "FAQs"}</Link></li>
+                    <li><Link href="/privacy-policy">{shouldAnimate ? <FutureText text="Privacy policy" delay={2000} speed={20} triggerOnVisible={false} /> : "Privacy policy"}</Link></li>
+                    <li><Link href="/terms-conditions">{shouldAnimate ? <FutureText text="Terms and conditions" delay={2000} speed={20} triggerOnVisible={false} /> : "Terms and conditions"}</Link></li>
+                    <li><Link href="/insights">{shouldAnimate ? <FutureText text="Shareholder Value Analytics" delay={2000} speed={20} triggerOnVisible={false} /> : "Shareholder Value Analytics"}</Link></li>
                 </ul>
             ),
             colSpan: 1,
@@ -323,13 +323,13 @@ const Footer: React.FC = () => {
                         )}
                     </h2>
                     <ul className="text-[1.5vh] leading-tight space-y-2">
-                        <li><a href="https://www.google.com">{shouldAnimate ? <FutureText text="Contact" delay={2000} speed={20} triggerOnVisible={false} /> : "Contact"}</a></li>
-                        <li><a href="https://www.openai.com">{shouldAnimate ? <FutureText text="Keynotes" delay={2000} speed={20} triggerOnVisible={false} /> : "Keynotes"}</a></li>
-                        <li><a href="https://www.github.com">{shouldAnimate ? <FutureText text="Join us" delay={2000} speed={20} triggerOnVisible={false} /> : "Join us"}</a></li>
-                        <li><a href="/faq">{shouldAnimate ? <FutureText text="FAQs" delay={2000} speed={20} triggerOnVisible={false} /> : "FAQs"}</a></li>
-                        <li><a href="/privacy-policy">{shouldAnimate ? <FutureText text="Privacy policy" delay={2000} speed={20} triggerOnVisible={false} /> : "Privacy policy"}</a></li>
-                        <li><a href="/terms-conditions">{shouldAnimate ? <FutureText text="Terms and conditions" delay={2000} speed={20} triggerOnVisible={false} /> : "Terms and conditions"}</a></li>
-                        <li><a href="https://www.github.com">{shouldAnimate ? <FutureText text="Shareholder Value Analytics" delay={2000} speed={20} triggerOnVisible={false} /> : "Shareholder Value Analytics"}</a></li>
+                        <li><Link href="/contact">{shouldAnimate ? <FutureText text="Contact" delay={2000} speed={20} triggerOnVisible={false} /> : "Contact"}</Link></li>
+                        <li><Link href="/keynotes">{shouldAnimate ? <FutureText text="Keynotes" delay={2000} speed={20} triggerOnVisible={false} /> : "Keynotes"}</Link></li>
+                        <li><Link href="/people">{shouldAnimate ? <FutureText text="Join us" delay={2000} speed={20} triggerOnVisible={false} /> : "Join us"}</Link></li>
+                        <li><Link href="/faq">{shouldAnimate ? <FutureText text="FAQs" delay={2000} speed={20} triggerOnVisible={false} /> : "FAQs"}</Link></li>
+                        <li><Link href="/privacy-policy">{shouldAnimate ? <FutureText text="Privacy policy" delay={2000} speed={20} triggerOnVisible={false} /> : "Privacy policy"}</Link></li>
+                        <li><Link href="/terms-conditions">{shouldAnimate ? <FutureText text="Terms and conditions" delay={2000} speed={20} triggerOnVisible={false} /> : "Terms and conditions"}</Link></li>
+                        <li><Link href="/insights">{shouldAnimate ? <FutureText text="Shareholder Value Analytics" delay={2000} speed={20} triggerOnVisible={false} /> : "Shareholder Value Analytics"}</Link></li>
                     </ul>
                 </div>
             </div>
