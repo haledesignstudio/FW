5df406668ce7b7c03c01455fa6102f5d16010794
Malcolm Scarrott <mscarrott@saao.ac.za>
2025-08-28 13:35:24 +0200

    main

--- src/app/api/draft-mode/disable/route.ts
+++ src/app/api/draft-mode/disable/route.ts
@@ -1,6 +1,7 @@
 import { draftMode } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
+export const runtime = 'edge';
 
 export async function GET(request: NextRequest) {
   (await draftMode()).disable();
